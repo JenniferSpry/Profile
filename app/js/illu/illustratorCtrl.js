97f8bdff5c9e63fe0614d415f4def70521a40e9b
jennifer spry <mail@jenniferspry.com>
2015-01-24 17:12:54 +0100

    added page description for crafts and illus

--- app/js/illu/illustratorCtrl.js
+++ app/js/illu/illustratorCtrl.js
@@ -2,10 +2,13 @@
 
 angular.module('jenniferSpry')
 .controller('IllustratorCtrl', ['$scope', 'IlluHttpService', function($scope, IlluHttpService) {
+
+  var pageDescription = "I grew up always creating fanart for Sailor Moon and Dragon Ball. In 2007 I was given the opportunity to draw a cover for a school book. Since then I have been illustrating on the side. My most ambitious and extensive project was the illustration for the ipad maths app <a href='http://clevermath.de/en/' target='_blank'>CleverMath</a>.";
  
   IlluHttpService.getIllus().then(
     function(responseData) {
       $scope.illus = responseData.data;
+      $scope.illus.unshift({title:pageDescription});
     }
   );
 
